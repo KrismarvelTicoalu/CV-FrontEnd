e57dac69afd64beda7f24564a6a009ac569102b2
Krismarvel Ticoalu <avelticoalu22@gmail.com>
2023-11-12 16:43:48 +0800

    add About, Education, Skills, Interests, Awards components and delete Experience section

---
+++ cv-KrismarvelTicoalu/src/components/About/index.jsx
@@ -0,0 +1,51 @@
+export const About = () => {
+  return (
+    <div>
+        <section className="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+            <div className="my-auto">
+                <h1 className="mb-0">Krismarvel
+                <span className="text-primary"> Ticoalu</span>
+                </h1>
+                <div className="subheading mb-5">3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·
+                <a href="mailto:name@email.com">name@email.com</a>
+                </div>
+                <p className="mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
+                <ul className="list-inline list-social-icons mb-0">
+                <li className="list-inline-item">
+                    <a href="#">
+                    <span className="fa-stack fa-lg">
+                        <i className="fa fa-circle fa-stack-2x" />
+                        <i className="fa fa-facebook fa-stack-1x fa-inverse" />
+                    </span>
+                    </a>
+                </li>
+                <li className="list-inline-item">
+                    <a href="#">
+                    <span className="fa-stack fa-lg">
+                        <i className="fa fa-circle fa-stack-2x" />
+                        <i className="fa fa-twitter fa-stack-1x fa-inverse" />
+                    </span>
+                    </a>
+                </li>
+                <li className="list-inline-item">
+                    <a href="#">
+                    <span className="fa-stack fa-lg">
+                        <i className="fa fa-circle fa-stack-2x" />
+                        <i className="fa fa-linkedin fa-stack-1x fa-inverse" />
+                    </span>
+                    </a>
+                </li>
+                <li className="list-inline-item">
+                    <a href="#">
+                    <span className="fa-stack fa-lg">
+                        <i className="fa fa-circle fa-stack-2x" />
+                        <i className="fa fa-github fa-stack-1x fa-inverse" />
+                    </span>
+                    </a>
+                </li>
+                </ul>
+            </div>
+            </section>
+    </div>
+  )
+}
---
+++ cv-KrismarvelTicoalu/src/components/Skills/index.jsx
@@ -0,0 +1,62 @@
+export const Skills = () => {
+  return (
+    <div>
+        <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+            <div className="my-auto">
+                <h2 className="mb-5">Skills</h2>
+                <div className="subheading mb-3">Programming Languages &amp; Tools</div>
+                <ul className="list-inline list-icons">
+                <li className="list-inline-item">
+                    <i className="devicons devicons-html5" />
+                </li>
+                <li className="list-inline-item">
+                    <i className="devicons devicons-css3" />
+                </li>
+                <li className="list-inline-item">
+                    <i className="devicons devicons-javascript" />
+                </li>
+                <li className="list-inline-item">
+                    <i className="devicons devicons-jquery" />
+                </li>
+                <li className="list-inline-item">
+                    <i className="devicons devicons-sass" />
+                </li>
+                <li className="list-inline-item">
+                    <i className="devicons devicons-less" />
+                </li>
+                <li className="list-inline-item">
+                    <i className="devicons devicons-bootstrap" />
+                </li>
+                <li className="list-inline-item">
+                    <i className="devicons devicons-wordpress" />
+                </li>
+                <li className="list-inline-item">
+                    <i className="devicons devicons-grunt" />
+                </li>
+                <li className="list-inline-item">
+                    <i className="devicons devicons-gulp" />
+                </li>
+                <li className="list-inline-item">
+                    <i className="devicons devicons-npm" />
+                </li>
+                </ul>
+                <div className="subheading mb-3">Workflow</div>
+                <ul className="fa-ul mb-0">
+                <li>
+                    <i className="fa-li fa fa-check" />
+                    Mobile-First, Responsive Design</li>
+                <li>
+                    <i className="fa-li fa fa-check" />
+                    Cross Browser Testing &amp; Debugging</li>
+                <li>
+                    <i className="fa-li fa fa-check" />
+                    Cross Functional Teams</li>
+                <li>
+                    <i className="fa-li fa fa-check" />
+                    Agile Development &amp; Scrum</li>
+                </ul>
+            </div>
+            </section>
+    </div>
+  )
+}
