d96bb5b1f6a20ede8f86f935a0abe0539de5d9cc
Krismarvel Ticoalu <avelticoalu22@gmail.com>
2023-11-12 20:51:55 +0800

    edit profile photo

--- cv-KrismarvelTicoalu/src/components/Navigation/index.jsx
+++ cv-KrismarvelTicoalu/src/components/Navigation/index.jsx
@@ -9,7 +9,7 @@ export const Navigation = () => {
             <a className="navbar-brand js-scroll-trigger" href="#page-top">
             <span className="d-block d-lg-none">Start Bootstrap</span>
             <span className="d-none d-lg-block">
-                <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="https://i.imgur.com/VQyVwVz.jpg" alt />
+                <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="https://i.imgur.com/PfdKFLy.png" alt />
             </span>
             </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
